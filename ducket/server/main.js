0c837376714db799e5ef1784ecc0b55c7de44b53
simbo1984 <62213297+simbo1984@users.noreply.github.com>
2022-05-06 18:35:46 -0400

    Delete ducket native

--- ducket/server/main.js
+++
@@ -1,5 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-import { SourcesCollection } from '/imports/api/sources/sources';
-import { LinksCollection } from '../imports/api/links/links';
-
-Meteor.startup(() => {});
